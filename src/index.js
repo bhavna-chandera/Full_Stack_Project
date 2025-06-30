216b65194febb99abec30046e6a447f095d28f14
bhavna-chandera <bhavnachandera2004@gmail.com>
2025-06-30 15:39:36 +0530

    add initial files for backend

--- src/index.js
+++ src/index.js
@@ -1,2 +1,53 @@
-// 4:42:16
+// 5:19:50 -> video lecture of backend course part 1
 
+// app with express through
+// database connection hoga mongoose k through
+
+// there are 2 approaches to connect to mongodb
+// 1. whole setup written in index.js
+// 2. separate file for database connection in db folder
+
+//import mongoose from "mongoose";
+//import { DB_NAME } from "./constants.js";
+//import connectDB from "./db/index.js";
+
+// there are 2 approaches to connect to mongodb
+// 1. whole setup written in index.js
+// 2. separate file for database connection in db folder
+
+// 1st approach
+// require("dotenv").config({ path: "./env" });
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+// import connectDB from "./db/index.js";
+// connectDB();
+
+/* 2nd approach */
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERRR:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR:", error);
+    throw err;
+  }
+})();
+*/
+
+require("dotenv").config({ path: "./env" });
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+connectDB();
